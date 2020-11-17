9547e85646261516a8adfa918e5d7376a5978f85
Ubuntu <ubuntu@ip-172-31-30-203.ec2.internal>
2020-11-17 14:35:12 +0000

    arquivo teste

---
+++ test2.py
@@ -0,0 +1,128 @@
+import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+import tf
+import math
+
+
+kp = 1
+ki = 0.5
+Int = 0
+kd = 0.5
+old_error = 0
+
+T = 0.1
+
+odom = Odometry()
+scan = LaserScan()
+
+rospy.init_node('cmd_node')
+
+# Auxiliar functions ------------------------------------------------
+def getAngle(msg):
+    quaternion = msg.pose.pose.orientation
+    quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+    euler = tf.transformations.euler_from_quaternion(quat)
+    yaw = euler[2]*180.0/math.pi
+    return yaw
+
+# CALLBACKS ---------------------------------------------------------
+def odomCallBack(msg):
+    global odom
+    odom = msg
+    
+def scanCallBack(msg):
+    global scan
+    scan = msg
+#--------------------------------------------------------------------
+
+# TIMER - Control Loop ----------------------------------------------
+def timerCallBack(event):
+    global kp, ki, kd
+    global Int
+    global old_error
+    """
+    yaw = getAngle(odom)
+    setpoint = -45
+    error = (setpoint - yaw)
+    
+    if abs(error) > 180:
+        if setpoint < 0:
+            error += 360 
+        else:
+            error -= 360
+    """
+    """
+    setpoint = (-1,-1)
+    position = odom.pose.pose.position
+    dist = setpoint[0] - position.x #math.sqrt((setpoint[0] - position.x)**2 + (setpoint[1] - position.y) **2)
+    
+    """
+    
+    if estado == 1:
+        setpoint = 0.5
+        
+        scan_len = len(scan.ranges)
+        
+        if scan_len > 0:
+            yaw = getAngle(odom)
+            
+            ind = scan.ranges.index(min(scan.ranges))
+            inc = 2*math.pi / scan_len
+            ang = (ind * inc * 180.0/math.pi) + yaw
+            if ang > 180:
+                ang -= 360
+                
+            error = (ang - yaw)
+            
+            if abs(error) > 180:
+                if setpoint < 0:
+                    error += 360 
+                else:
+                    error -= 360
+                    
+            print(ang, yaw, error)
+            
+            delta_e = error - old_error
+            old_error = error
+            
+            P = kp*error
+            Int += error*T
+            I = Int * ki
+            D = delta_e * kd
+            
+            control = P+I+D
+            if control > 1:
+                control = 1
+            elif control < -1:
+                control = -1
+        else:
+            control = 0    
+            error = 1
+        
+        msg = Twist()
+        msg.angular.z = control
+        pub.publish(msg)
+        
+        if abs(error) < 1:
+            Int = 0
+            estado = 2
+    
+    elif estado == 2:
+        read = min(scan.ranges)
+        
+        # PID
+        
+        msg = Twist()
+        msg.linear.x = control
+        pub.publish(msg)
+        
+
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
+scan_sub = rospy.Subscriber('/scan', LaserScan, scanCallBack)
+
+timer = rospy.Timer(rospy.Duration(T), timerCallBack)
+
+rospy.spin()
